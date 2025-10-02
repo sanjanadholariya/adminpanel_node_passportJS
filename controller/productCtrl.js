284f6a8815fa88f3144bc792cfeb189f33191428
KBthumar180 <kavya.thummar@gmail.com>
2025-10-02 12:37:27 +0530

    02/10/2025

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -2,6 +2,8 @@ const categoryModel = require('../model/categoryModel')
 const subcategoryModel = require('../model/subcategoryModel')
 const extraCategoryModel = require('../model/extraCategoryModel')
 const productModel = require('../model/productModel')
+const path = require('path')
+const fs = require('fs')
 
 module.exports.addProductPage = async(req , res) => {
     try{
@@ -37,7 +39,7 @@ module.exports.addProduct = async(req , res) => {
 
 module.exports.viewProductPage = async(req , res) => {
     try{
-        console.log("viewProductPage")
+        // console.log("viewProductPage")
         const allProduct = await productModel.find().populate('category').populate('subcategory').populate('extraCategory')
         // console.log(allProduct)
         return res.render('product/viewProductPage',{allProduct})
@@ -52,6 +54,66 @@ module.exports.viewSingleProductPage = async(req , res) => {
         const single = await productModel.findById(req.params.id).populate('category').populate('subcategory').populate('extraCategory')
         // console.log(single)
         return res.render('product/viewSingleProductPage',{single})
+    }catch(err){
+        console.log(err)
+        return res.redirect('/admin')
+    }
+}
+
+module.exports.deleteProduct = async(req , res) => {
+    try{
+        console.log(req.params.id)
+        const single = await productModel.findById(req.params.id)
+        if(single){
+            if(single.productImage){
+                const oldPath = path.join(__dirname,'../uploads/productImage',single.productImage);
+                console.log(oldPath)
+                fs.unlinkSync(oldPath)
+            }
+            await productModel.findByIdAndDelete(req.params.id)
+            console.log("Product Is Delete")
+            return res.redirect('/product/viewProductPage')
+        }else{
+            console.log("There Is No Data Found Ny This Id!!")
+        }
+    }catch(err){
+        console.log(err)
+        return res.redirect('/admin')
+    }
+}
+
+module.exports.editProductPage = async(req , res) =>{
+    try{
+        console.log(req.params.id)
+        const singleProduct = await productModel.findById(req.params.id).populate('category').populate('subcategory').populate('extraCategory')
+
+        return res.render('product/editProductPage',{singleProduct})
+    }catch(err){
+        console.log(err)
+        return res.redirect('/admin')
+    }
+}
+
+module.exports.editProduct = async(req , res) =>{
+    try{
+        // console.log("edit Id",req.params.id)
+        const single = await productModel.findById(req.params.id)
+        // console.log(single)
+        console.log(req.body)
+        if(single){
+           if(req.file){
+            if(single.productImage){
+                const oldPath = path.join(__dirname,'../uploads/productImage',single.productImage)
+                fs.unlinkSync(oldPath)
+            }
+            req.body.productImage = req.file.filename;
+           }
+           await productModel.findByIdAndUpdate(req.params.id , {productName : req.body.productName , description : req.body.description , price : req.body.price , productImage : req.body.productImage , quantity : req.body.quantity});
+           console.log("Product Data Updated..")
+        }else{
+            console.log("There Is No Data By This Id !!")
+        }
+        return res.redirect("/product/viewProductPage")
     }catch(err){
         console.log(err)
         return res.redirect('/admin')
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const productModel = require('../model/productModel')
 const routes = express.Router()
-const { addProductPage, addProduct, viewProductPage, viewSingleProductPage } = require('../controller/productCtrl');
+const { addProductPage, addProduct, viewProductPage, viewSingleProductPage, deleteProduct, editProductPage, editProduct } = require('../controller/productCtrl');
 const passport = require('passport');
 
 routes.get('/addProductPage',addProductPage)
 routes.post('/addProduct',productModel.productImageUpload,addProduct)
 routes.get('/viewProductPage',productModel.productImageUpload,viewProductPage)
 routes.get('/viewSingleProductPage/:id',productModel.productImageUpload,viewSingleProductPage)
+routes.get('/deleteProduct/:id',deleteProduct)
+routes.get('/editProductPage/:id',productModel.productImageUpload,editProductPage)
+routes.post('/editProduct/:id',productModel.productImageUpload,editProduct)
 
 module.exports = routes;
